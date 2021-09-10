d693dde630f195e9ec82fd39d3439322e27aeb6a
Rajarshi Bardhan <rajarshi.ae@gmail.com>
2021-09-10 16:08:01 +0800

    Add project files.

---
+++ MovieBookingApi/Models/BookingItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieBookingApi.Models
+{
+    public class BookingItem
+    {
+        public long Id { get; set; }
+        public DateTime ShowTime { get; set; }
+        public string Venue { get; set; }
+        public string Currency { get; set; }
+        public double Price { get; set; }
+    }
+}
